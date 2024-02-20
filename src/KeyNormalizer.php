812f40791ee5ebde3a5912871fe9ffc0d45bc046
Anton Petrenko <antptrnk@gmail.com>
2024-02-20 16:20:17 +0200

    v1.1.0

--- src/KeyNormalizer.php
+++ src/KeyNormalizer.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 namespace KeyNormalizer;
 
+use function lcfirst;
+use function preg_replace;
+use function str_replace;
+use function strtolower;
+use function ucwords;
+
 class KeyNormalizer
 {
-    public static function normalize(string $input): string
+    public static function toCamelCase(string $key): string
     {
-        return \lcfirst(\str_replace(' ', '', \ucwords(\str_replace('_', ' ', $input))));
+        return lcfirst(str_replace('_', '', ucwords($key, '_')));
+    }
+
+    public static function toSnakeCase(string $key): string
+    {
+        return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $key));
     }
 }
